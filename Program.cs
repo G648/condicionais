41f5d5b98fa70a5550b91fe6ddbada7b4c1c23e1
Guilherme_Amorim <cezarguilherme03@gmail.com>
2021-08-24 17:00:13 -0300

    condicionais

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("calculadora de dois digitos");
+           //writeline pula uma linha para escrever
+            Console.WriteLine("Por favor digite o primeiro digito");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("digite o segundo numero");
+            double num2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("ok, qual operação deseja fazer ?");
+            Console.WriteLine("digite 1 para somar");
+            Console.WriteLine("digite 2 para subtrair");
+            Console.WriteLine("digite 3 para dividir");
+            Console.WriteLine("digite 4 para multiplicar");
+             int resposta = int.Parse(Console.ReadLine());
+            
+            double resultado = 0;
+            bool validacao = true;
+            //bool pode ser true ou false
+            
+                if (resposta == 1){
+                    resultado = num1 + num2;
+                }else if(resposta ==2){
+                    resultado = num1 - num2;
+                }else if(resposta ==3){
+                    resultado = num1 / num2;
+                }else if(resposta ==4){
+                    resultado = num1 * num2;
+                }else{
+                    Console.WriteLine("o numero está incorreto");
+                    validacao = false;  
+                }
+                if(validacao == true){
+                     Console.WriteLine("o resultado = "+resultado);
+                }
+        }
+    }
+}
